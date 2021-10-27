e320419478c1282bc9871df73d95c7e1b7b56553
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2021-10-27 15:55:48 -0300

    exercícios em dupla

---
+++ Aulas JS/Aula 4/exec 1.js
@@ -0,0 +1,19 @@
+// 1) Crie uma função que inverta um número; (NÃO pode usar a função revert())
+//    Exemplo: minhaFuncao(370914) // retorno esperado: 419073;
+
+
+
+var lista = ['3','7','0','9','1', '4'];
+var lista2 = []; 
+
+function revertArray (myList) {
+    var tamanhoArray = myList.length
+    for (i = 0; i <= tamanhoArray; i++) {
+        var item = myList.pop()
+        if (typeof item !== 'undefined'){
+            lista2.push(item)
+        }
+       
+    }
+    return lista2
+}
